8927df6bd940b772e2da2e58a7f2ce0854be576f
KrystianChaim <chaimkrystian@gmail.com>
2025-12-08 23:39:38 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,55 +1,34 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import HomeScreen from './screens/HomeScreen';
-import QuizScreen from './screens/QuizScreen';
-import ResultsScreen from './screens/ResultsScreen';
-
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from "./screens/SplashScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import QuizScreen from "./screens/QuizScreen";
+import ResultsScreen from "./screens/ResultsScreen";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
-// ---------------- Drawer zawierający ekrany ------------------
-
 function DrawerMenu() {
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        drawerActiveTintColor: "#0A84FF",
-        drawerLabelStyle: { fontSize: 18 },
-      }}
-    >
-      <Drawer.Screen 
-        name="Home" 
-        component={HomeScreen} 
-        options={{ title: "Ekran główny" }} 
-      />
-
-      <Drawer.Screen 
-        name="Quiz" 
-        component={QuizScreen} 
-        options={{ title: "Quiz" }} 
-      />
-
-      <Drawer.Screen 
-        name="Results" 
-        component={ResultsScreen} 
-        options={{ title: "Wyniki" }} 
-      />
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreen} options={{ title: "Ekran główny" }} />
+      <Drawer.Screen name="Quiz" component={QuizScreen} options={{ title: "Quiz" }} />
+      <Drawer.Screen name="Results" component={ResultsScreen} options={{ title: "Wyniki" }} />
     </Drawer.Navigator>
   );
 }
-
-// ---------------- Root App ------------------
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* Cała aplikacja to Drawer */}
+          <Stack.Screen name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
           <Stack.Screen name="Root" component={DrawerMenu} />
         </Stack.Navigator>
       </NavigationContainer>
