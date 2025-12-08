6547e783c77b2b084e6cc8c690b01e7b1287ccce
KrystianChaim <chaimkrystian@gmail.com>
2025-12-08 23:40:43 +0100

    Add files via upload

---
+++ data/tasks.js
@@ -0,0 +1,37 @@
+export const tasks = [
+  {
+    question: "Który wódz prowadził wojnę domową z Sullą?",
+    duration: 30,
+    type: "historia",
+    answers: [
+      { content: "Lucjusz Cynna", isCorrect: true },
+      { content: "Juliusz Cezar", isCorrect: false },
+      { content: "Lucjusz Murena", isCorrect: false },
+      { content: "Marek Krassus", isCorrect: false }
+    ]
+  },
+
+  {
+    question: "Stolica Hiszpanii to:",
+    duration: 20,
+    type: "geografia",
+    answers: [
+      { content: "Madryt", isCorrect: true },
+      { content: "Barcelona", isCorrect: false },
+      { content: "Walencja", isCorrect: false },
+      { content: "Sewilla", isCorrect: false }
+    ]
+  },
+
+  {
+    question: "Kto wynalazł żarówkę?",
+    duration: 25,
+    type: "technologia",
+    answers: [
+      { content: "Thomas Edison", isCorrect: true },
+      { content: "Nikola Tesla", isCorrect: false },
+      { content: "Graham Bell", isCorrect: false },
+      { content: "Newton", isCorrect: false }
+    ]
+  }
+];
